eebac0b42e40e8263de96571710a381dfa2649f6
icodedao <thanhphongtran.it@gmail.com>
2020-05-07 20:07:28 +0700

    search

--- admin/classes/category.php
+++ admin/classes/category.php
@@ -61,6 +61,12 @@
 			$result = $this->db->select($query);
 			return $result;
 		}
+		public function get_list($sql)
+		{
+			$query = $sql;
+			$result = $this->db->select($query);
+			return $result;
+		}
 
 		public function edit_category($tenLoai, $id) //Sửa danh mục
 		{
